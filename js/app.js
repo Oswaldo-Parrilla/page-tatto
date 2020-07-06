eb3ebb323a87b9e6a26c458f5dbec0408d259e0c
oswaldo <oswaparrilla@gmail.com>
2020-07-05 21:09:01 -0600

    avance de pagina web al 60%

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//CAROUSEL
+// document.addEventListener('DOMContentLoaded', () =>  {
+//     const elems = document.querySelectorAll('.carousel');
+//     M.Carousel.init(elems, {
+//         duration: 500,
+//         dist: -80,
+//         shift: 5,
+//         numVisible: 3,
+//         fullWidth: true,
+//         indicators: true
+        
+//     });
+// });
